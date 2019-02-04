6b76bfb87b6c0858a190c068d70572f3d8d96a40
Justin <greyepoxy2@gmail.com>
2019-02-04 14:56:01 -0800

    fix: extract-function no 3

--- exercises/typescript/mechanics/extract-function/3/start.ts
+++ exercises/typescript/mechanics/extract-function/3/start.ts
@@ -1,4 +1,4 @@
-export function mainStart(): void {
+export function main(): void {
   for (let i = 1; i <= 100; i++) {
     let result = '';
 
@@ -18,33 +18,4 @@ export function mainStart(): void {
 
     console.log(result);
   }
-}
-
-export function main(): void {
-  for (let i = 1; i <= 100; i++) {
-    let result = '';
-
-    result = maybeAppendToResultFizzBuzzOrOutputInput(i, result);
-
-    console.log(result);
-  }
-}
-
-function maybeAppendToResultFizzBuzzOrOutputInput(
-  i: number,
-  result: string
-): string {
-  if (i % 3 === 0) {
-    result += 'Fizz';
-  }
-
-  if (i % 5 === 0) {
-    result += 'Buzz';
-  }
-
-  if (result === '') {
-    console.log(i.toString());
-  }
-
-  return result;
 }
