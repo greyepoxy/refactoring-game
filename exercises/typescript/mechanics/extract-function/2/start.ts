94d8c2d9907165a9bd44d1b9058ca4a332884db0
Justin <greyepoxy2@gmail.com>
2019-02-06 15:03:14 -0800

    r move method

--- exercises/typescript/mechanics/extract-function/2/start.ts
+++ exercises/typescript/mechanics/extract-function/2/start.ts
@@ -1,18 +1,18 @@
+const getText = (i: number) => {
+  let result = '';
+
+  if (i % 3 === 0) {
+    result += 'Fizz';
+  }
+
+  if (i % 5 === 0) {
+    result += 'Buzz';
+  }
+  return result;
+};
 export function main(): void {
   for (let i = 1; i <= 100; i++) {
     // Extract this block into a function in the module scope
-    const getText = (i: number) => {
-      let result = '';
-
-      if (i % 3 === 0) {
-        result += 'Fizz';
-      }
-
-      if (i % 5 === 0) {
-        result += 'Buzz';
-      }
-      return result;
-    };
     const result = getText(i);
     //
 
