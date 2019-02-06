aa3c4b0a94acbfa505379184bc8ec985ca52a4de
Justin <greyepoxy2@gmail.com>
2019-02-06 14:59:50 -0800

    r renamed foo to getText

--- exercises/typescript/mechanics/extract-function/2/start.ts
+++ exercises/typescript/mechanics/extract-function/2/start.ts
@@ -1,7 +1,7 @@
 export function main(): void {
   for (let i = 1; i <= 100; i++) {
     // Extract this block into a function in the module scope
-    const result = (() => {
+    const getText = () => {
       let result = '';
 
       if (i % 3 === 0) {
@@ -12,7 +12,8 @@ export function main(): void {
         result += 'Buzz';
       }
       return result;
-    })();
+    };
+    const result = getText();
     //
 
     if (result === '') {
