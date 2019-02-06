585b8ed4b57839eadaa34f9d6d53fa2a03160028
Justin <greyepoxy2@gmail.com>
2019-02-06 14:36:00 -0800

    r moved printInstructions

--- exercises/typescript/mechanics/extract-function/1/start.ts
+++ exercises/typescript/mechanics/extract-function/1/start.ts
@@ -1,8 +1,12 @@
-export function main(): void {
-  // Extract this block into a function in the module scope
+const printInstructions = () => {
   console.log('For numbers one through 100 will print out Fizz if the number');
   console.log('is divisible by 3, Buzz if it is divisible by 5, FizzBuzz if');
   console.log('it is divisible by both, else will just print the number.');
+};
+
+export function main(): void {
+  // Extract this block into a function in the module scope
+  printInstructions();
   //
 
   for (let i = 1; i <= 100; i++) {
